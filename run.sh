acd21262d563bdb95d00eaa03dab37ed0ce305ba
lucas <lucasjgordon@gmail.com>
2017-01-08 16:52:06 +0000

    Moves tests to tests directory.

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 rm -rf dist && mkdir dist # cleans dist directory
 javac ./lib/*.java -d dist # compiles interfaces
 javac ./*.java -cp dist -d dist # compiles classes
+javac ./tests/*.java -cp dist -d dist # compiles tests
 java -cp dist Launcher # runs tests
