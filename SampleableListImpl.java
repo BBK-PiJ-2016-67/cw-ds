c861e4dedea6ca480faf3ccdde944ed079afe50d
lucas <lucasjgordon@gmail.com>
2017-01-08 16:27:37 +0000

    Adds comments to sampleable list implementation class and stack implementation class.

--- SampleableListImpl.java
+++ SampleableListImpl.java
@@ -1,11 +1,16 @@
 /**
- * 
+ * SampleableListImpl is a class extending {@see ArrayList} and
+ * implementing {@see SampleableList}.
  *
  * @author BBK-PiJ-2016-67
  */
 public class SampleableListImpl extends ArrayList implements SampleableList {
 	/**
+	 * Returns a {@see SampleableListImpl} containing the first,
+	 * third, fifth etc elements of the list.
 	 *
+	 * @return 	a list if elements are present, otherwise an empty
+	 *			list
 	 */
 	public SampleableList sample() {
 		SampleableListImpl sampleableList = new SampleableListImpl();
--- StackImpl.java
+++ StackImpl.java
@@ -1,23 +1,30 @@
 /**
- * 
+ * StackImpl is a class implementing {@see AbstractStack}.
  *
  * @author BBK-PiJ-2016-67
  */
 public class StackImpl extends AbstractStack {
 	/**
+	 * Initialises the stack with a {@see List}.
 	 *
+	 * @param list 	a list of elements
 	 */
 	public StackImpl(List list) {
 		super(list);
 	}
 	/**
+	 * Indicates whether the stack is empty or not.
 	 *
+	 * @return 	true if the stack is empty, false otherwise
 	 */
 	public boolean isEmpty() {
 		return this.size() == 0;
 	}
 	/**
+	 * Returns and removes the element at the top of the stack.
 	 *
+	 * @return 	a {@see ReturnObjectImpl} containing a return value
+	 *			if successful, otherwise an error
 	 */
 	public ReturnObject pop() {
 		if (this.isEmpty()) {
@@ -29,19 +36,26 @@ public class StackImpl extends AbstractStack {
 		return topObject;
 	}
 	/**
+	 * Adds an element to the stack.
 	 *
+	 * @param item	the element to be added
 	 */
 	public void push(Object item) {
 		super.internalList.add(item);
 	}
 	/**
+	 * Indicates the size of the stack.
 	 *
+	 * @return 	the size of the stack
 	 */
 	public int size() {
 		return super.internalList.size();
 	}
 	/**
+	 * Returns the element at the top of the stack.
 	 *
+	 * @return 	a {@see ReturnObjectImpl} containing a return value
+	 *			if successful, otherwise an error
 	 */
 	public ReturnObject top() {
 		if (this.isEmpty()) {
